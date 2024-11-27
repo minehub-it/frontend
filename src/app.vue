5011168eb7173a8a0edead3127d1fdd51d21056c
dxlliv <hello@dxlliv.dev>
2024-11-27 10:32:47 +0100

    Move app init functions to global middleware

--- src/app.vue
+++ src/app.vue
@@ -7,14 +7,5 @@
 <script setup lang="ts">
 // nuxt layer
 const serverListStore = useServerListStore()
-const serverCategoryStore = useServerCategoryStore()
-const serverStorageStore = useServerStorageStore()
 
-await serverCategoryStore.initialize()
-await serverListStore.fetchFromContent()
-
-onBeforeMount(async () => {
-  await serverStorageStore.restore()
-  await serverListStore.fetchPlayers()
-})
 </script>
