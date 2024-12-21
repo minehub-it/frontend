e17503991d7bb3c38f7316ffaba51a1cc174b8d1
dxlliv <hello@dxlliv.dev>
2024-12-21 02:04:16 +0100

    Improve server list

--- src/app.vue
+++ src/app.vue
@@ -3,3 +3,20 @@
     <NuxtLayout />
   </v-app>
 </template>
+
+<script setup lang="ts">
+// server list layer
+{
+  const serverListStore = useServerListStore()
+  const serverCategoryStore = useServerCategoryStore()
+  const serverStorageStore = useServerStorageStore()
+
+  await serverCategoryStore.initialize()
+  await serverListStore.fetchFromContent()
+
+  onMounted(async () => {
+    await serverStorageStore.restore()
+    await serverListStore.fetchPlayers()
+  })
+}
+</script>
