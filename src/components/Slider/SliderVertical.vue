6dee7ad95482f691af7b6a4eb093118c71ebb94f
dxlliv <hello@dxlliv.dev>
2024-12-23 15:41:51 +0100

    Enable touchpad scroll on SliderVertical.vue

--- src/components/Slider/SliderVertical.vue
+++ src/components/Slider/SliderVertical.vue
@@ -1,12 +1,7 @@
-<script setup lang="ts">
-import {Mousewheel} from "swiper/modules";
-</script>
-
 <template>
   <swiper-container
       direction="vertical"
-      mousewheel allow-touch-move
-      :modules="[Mousewheel]"
+      allow-touch-move mousewheel-force-to-axis="true"
       class="minehub-slider-vertical"
   >
     <slot />
