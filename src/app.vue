54682b250ef5aed6e03708899dd054e6422476fb
dxlliv <hello@dxlliv.dev>
2025-03-13 20:57:00 +0100

    Aah there was a reason things were there

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,21 @@
 <template>
   <v-app>
-    
     <MainHeader class="hidden-md-and-up"/>
 
     <NuxtLayout/>
-
   </v-app>
 </template>
+
+<script setup lang="ts">
+const serverListStore = useServerListStore()
+const serverCategoryStore = useServerCategoryStore()
+
+await serverCategoryStore.initialize()
+await serverListStore.fetchFromContent()
+
+await serverListStore.fetchPlayers()
+
+onMounted(async () => {
+  await serverListStore.fetchPlayers()
+})
+</script>
